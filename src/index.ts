adbebb56213f0200343487ba6efd85dbf801185a
eliJI <elijahingr@gmail.com>
2023-10-22 23:42:33 -0400

    fixed logic. container and graphics list are NOT the same

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,39 @@ const CENTER_Y = height/2
 const app = new PIXI.Application<HTMLCanvasElement>({width: width, height: height});
 document.body.appendChild(app.view);
 
-app.ticker.add() => {
+class Planet {
+    degreesPerTick: number;
+    theta: number = 0;
+    radius: number;
+    circle: PIXI.Graphics;
 
+
+    constructor(degreesPerTick: number, radius: number) {
+        this.degreesPerTick = degreesPerTick;
+        this.radius = radius;
+        this.circle = new PIXI.Graphics();
+    }
+
+    init() {
+        this.circle.beginFill(0x4287f5);
+        this.circle.drawCircle(0, 0, 10);
+        app.stage.addChild(this.circle);
+    }
+
+    draw() {
+        this.circle.x = CENTER_X + this.radius * Math.cos(this.theta)
+        this.circle.y = CENTER_Y + this.radius * Math.sin(this.theta);
+        this.theta = this.theta + this.degreesPerTick;
+    }
 }
+
+let center = new PIXI.Graphics()
+center.beginFill(0x4287f5);
+center.drawCircle(CENTER_X, CENTER_Y, 20)
+app.stage.addChild(center);
+
+let planet = new Planet(.05, 80);
+planet.init();
+app.ticker.add(() => {
+    planet.draw();
+});
